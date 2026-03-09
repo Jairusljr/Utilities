92542a9bcbdedd949adf78334a5450de37b54d42
Jairusljr <jairusleung12@gmail.com>
2026-03-09 17:12:46 +0800

    Add JUnit Tests

--- src/test/java/org/utils/stringutils/StringParserTest.java
+++ src/test/java/org/utils/stringutils/StringParserTest.java
@@ -1,4 +1,30 @@
+package org.utils.stringutils;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.utils.exceptions.InvalidStringInputException;
+
 class StringParserTest {
-  
+
+    // methodToTest_input_expectedOutput
+    @Test
+    void splitString_oneSeparator_twoParts() throws InvalidStringInputException {
+        StringParser sp = new StringParser();
+        String inputString = "Hello | World!";
+        String separator = "\\|";
+        List<String> stringParts = sp.splitString(inputString, separator);
+        assertEquals(2,stringParts.size());
+    }
+
+    // failure
+    @Test
+    void splitString_oneSeparator_null() {
+        StringParser sp = new StringParser();
+        String inputString = null;
+        String separator = "\\|";
+
+        assertThrows(InvalidStringInputException.class,
+                () -> sp.splitString(inputString, separator));
+    }
 }
